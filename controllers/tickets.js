3410a72a302f0f07eb98e6e2992e78017689d09c
Lincoln Youree <lincolnyouree@Lincolns-MacBook-Air.local>
2020-02-27 15:59:06 -0600

    Seats Displaying EJS

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -2,10 +2,10 @@ const Ticket = require('../models/ticket');
 const Flight = require('../models/flight');
 
 module.exports = {
-    new: newTicket,
+    // new: newTicket,
     addToBooking,
     index,
-    show,
+    
     create
 };
 
@@ -17,36 +17,31 @@ function index(req, res, next) {
 }
 
 function addToBooking(req, res) {
-    Flight.findById(req.params.id, function(err, flight) {
-        Ticket.find({flight: flight._id}, function(err, tickets) {
-            res.render('tickets/new', {
-                title: 'Add Ticket',
-                tickets
-        });
-    });
-})
-}
+  console.log(req.params.id)
+  Flight.findById(req.params.id, function(err, flight) {
+      res.render('tickets/new', {title: 'Add Ticket', flight});
+  });
+} ;
 
-function newTicket(req, res) {
-    Ticket.find({}, function(err, tickets) {
-      res.render('tickets/new', {
-        title: 'Add Ticket',
-        tickets
-      });
-    });
-  }
+// function newTicket(req, res) {
+//     Flight.findById(req.params.id, function(err, flight) {
+//       res.render('tickets/new', {
+//         title: 'Add Ticket',
+//         tickets
+//       });
+//     });
+//   }
 
-  function show(req, res) {
-    Ticket.findById(req.params.id, function(err, ticket) {
-      res.render('tickets/show', {title: 'Flight Details', ticket})
-    });
-  }
+  // function show(req, res) {
+  //   Ticket.findById(req.params.id, function(err, ticket) {
+  //     res.render('tickets/show', {title: 'Flight Details', ticket})
+  //   });
+  // }
 
   function create(req, res) {
-    Flight.findById(req.params.id, function(err, flight) {
-      flight.tickets.push(req.body);
-      flight.save(function(err) {
-        res.redirect(`/flights/${flight._id}`);
-      });
-    });
+    req.body.flight = req.params.id;
+    Ticket.create(req.body, function(err, tickets) {
+      console.log(tickets)
+      res.redirect(`/flights/${req.params.id}`)
+    })
   }
--- routes/tickets.js
+++ routes/tickets.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const ticketsCtrl = require('../controllers/tickets');
 
-router.get('/tickets/new', ticketsCtrl.new);
-router.post('/tickets', ticketsCtrl.create);
-router.post('/flights/:id/tickets/new', ticketsCtrl.addToBooking);
+// router.get('/tickets/new', ticketsCtrl.new);
+router.post('/flights/:id/tickets', ticketsCtrl.create);
+router.get('/flights/:id/tickets/new', ticketsCtrl.addToBooking);
 
 module.exports = router;
